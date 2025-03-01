fbac560d0249842d42deba2c6129685f83bb004a
jsw <jeongseongwoo83@daum.net>
2025-03-02 00:47:36 +0900

    [feat] tailwindcss설치

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import TodoInput from "./components/TodoInput";
 import TodoList from "./components/TodoList";
+import "./styles/App.css";
 
 export default function App() {
   return(
     // TODO UI개선하기
     <main style={{textAlign: 'center', marginTop: '50px'}}>
-      <h1>📝 Vite Todo List</h1>
+      <h1 className="ext-3xl font-bold underline">📝 Vite Todo List</h1>
       <TodoInput />
       <TodoList />
     </main>
