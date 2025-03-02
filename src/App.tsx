694f47646ea2fc182f6fd481f6511c3cfb8bd2c4
jsw <jeongseongwoo83@daum.net>
2025-03-02 21:14:43 +0900

    [feat] css 추가

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import "./styles/main.scss"
 export default function App() {
   return(
     // TODO UI개선하기
-    <main style={{textAlign: 'center', marginTop: '50px'}}>
+    <main>
       <h1 className="ext-3xl font-bold underline">📝 Vite Todo List</h1>
       <TodoInput />
       <TodoList />
--- src/components/TodoInput.tsx
+++ src/components/TodoInput.tsx
@@ -1,5 +1,22 @@
 import { JSX, useState } from "react"
 import { useTodoStore } from "../store/todoStore";
+import styled from "styled-components";
+
+const StyledInput = styled.input`
+    width: 50%;
+    padding: 12px;
+    border: 2px solid #ddd;
+    border-radius: 8px;
+    font-size: 16px;
+    outline: none;
+    transition: all 0.3s ease-in-out;
+
+    &:focus {
+        border-color: #6366f1;
+        box-shadow: 0 0 5px rgba(99, 102, 241, 0.5);
+    }
+`
+
 
 export default function TodoInput(): JSX.Element {
     const [text, setText] = useState<string>('');
@@ -14,7 +31,7 @@ export default function TodoInput(): JSX.Element {
 
     return (
         <div>
-            <input type="text" value={text} onChange={(e)=> setText(e.target.value)} placeholder="뭐라도 좀 해라;;"/>
+            <StyledInput type="text" value={text} onChange={(e)=> setText(e.target.value)} placeholder="뭐라도 좀 해라;;"/>
             <button onClick={handleAddTodo}>추가</button>
         </div>
     )
